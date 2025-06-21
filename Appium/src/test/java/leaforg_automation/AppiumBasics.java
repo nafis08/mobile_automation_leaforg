2fcf54998a28e9fc2e52ccfa4906a0881114d8fc
nafis08 <nafis.saqib@gmail.com>
2025-06-21 03:11:59 +0200

    Environment setup completed

--- Appium/src/test/java/leaforg_automation/AppiumBasics.java
+++ Appium/src/test/java/leaforg_automation/AppiumBasics.java
@@ -1,5 +1,6 @@
 package leaforg_automation;
 
+import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -8,19 +9,30 @@ import org.testng.annotations.Test;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
 
 public class AppiumBasics {
 	@Test
 	public void AppiumTest() throws MalformedURLException, URISyntaxException {
 		
+		//Start Appium Server automatically before the test
+		AppiumDriverLocalService service = new AppiumServiceBuilder()
+				.withAppiumJS(new File("//usr//local//lib//node_modules//appium//build//lib//main.js"))
+				.withIPAddress("127.0.0.1").usingPort(4723).build();
+		service.start();
+		
 		//Device connection setup through UiAutomator2
 		UiAutomator2Options options = new UiAutomator2Options();
 		options.setDeviceName("TestDevice API36");
-		options.setApp("//Users//zarintasnim//Desktop//leafrog_mobile_automation//Appium//src//test//java//app_installer//leaforg.apk");
+		options.setApp("/Users/zarintasnim/git/repository/Appium/src/test/java/app_installer/leaforg.apk");
 		
-		//Android driver initiation and server connection setup
-		AndroidDriver driver = new AndroidDriver(new URI("http://127.0.0.1:4723/").toURL(), options);
+		//Android driver initiation and server connection
+		AndroidDriver driver = new AndroidDriver(new URI("http://127.0.0.1:4723").toURL(), options);
 		driver.quit();
+		
+		//Stop the server
+		service.stop();
 		
 		
 	}
